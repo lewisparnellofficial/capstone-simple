af235c9e0fdc7333c81cff6c064611dfe0d51541
Lewis Parnell <lewis.g.parnell@gmail.com>
2025-10-19 19:15:23 -0700

    Init

---
+++ download.ps1
@@ -0,0 +1,20 @@
+$zip_url = "http://205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/CIC-IDS-2017/CSVs/MachineLearningCSV.zip"
+$md5_url = "http://205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/CIC-IDS-2017/CSVs/MachineLearningCSV.md5"
+
+$zip_path = ".\data\MachineLearningCSV.zip"
+$md5_path = ".\data\MachineLearningCSV.md5"
+
+if (-not (Test-Path $zip_path)) {
+	Invoke-WebRequest -Uri $zip_url -OutFile $zip_path
+}
+
+Invoke-WebRequest -Uri $md5_url -OutFile $md5_path
+
+$zip_hash = (Get-FileHash -Path $zip_path -Algorithm MD5).Hash
+$expected_hash = ((Get-Content -Path $md5_path) -split '\s')[0]
+if ($zip_hash -ne $expected_hash) {
+	Write-Host "The zip file is corrupt. Redownload the file."
+	exit 1
+}
+
+Expand-Archive -Force -Path $zip_path -DestinationPath ".\data\"
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from capstone-simple!")
+
+
+if __name__ == "__main__":
+    main()
